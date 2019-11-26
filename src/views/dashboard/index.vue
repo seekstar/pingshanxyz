ba53fa1b568335fa6b8eb4b5ee8483d4c6b71416
zszengshang <1511557654@qq.com>
2019-11-26 19:38:16 +0800

    bug fix 添加导出excel

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -162,10 +162,14 @@
         <box-card />
       </el-col> -->
     </el-row>
+    <el-tooltip placement="top" content="返回顶部">
+      <back-to-top :custom-style="myBackToTopStyle" :visibility-height="300" :back-position="50" transition-name="fade" />
+    </el-tooltip>
   </div>
 </template>
 
 <script>
+import BackToTop from '@/components/BackToTop'
 import GithubCorner from "@/components/GithubCorner";
 import PanelGroup from "./components/PanelGroup";
 import PieChart from "./components/PieChart";
@@ -504,16 +508,26 @@ export default {
     SunburstChart,
     DatePicker,
     DatePickerQuarter,
-    PingShanMap
+    PingShanMap,
+    BackToTop
   },
   data() {
     return {
+      myBackToTopStyle: {
+        right: '50px',
+        bottom: '50px',
+        width: '40px',
+        height: '40px',
+        'border-radius': '4px',
+        'line-height': '45px', // 请保持与高度一致以垂直居中 Please keep consistent with height to center vertically
+        background: '#e7eaf1'// 按钮的背景颜色 The background color of the button
+      },
       allErrorLog: [],
       BarChartData: ChartData.types_street,
       PieChartData: ChartData.properties,
       SunburstChartData: ChartData.status_type,
       pingShanMapData: ChartData.pingShanMapData,
-
+      count:0,
       lineChartXNames: [],
       lineChartDataTable: [
         //name: '求决',
