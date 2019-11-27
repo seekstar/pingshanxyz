772bf5490f850f20beb864dfe2e33f965b89a84a
searchstar <632863986@qq.com>
2019-11-27 15:12:20 +0800

    登录页面背景图高斯模糊，添加logo

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,92 +1,93 @@
 <template>
   <div class="login-container">
-    <div class="aside">
-      <el-form
-        ref="loginForm"
-        :model="loginForm"
-        :rules="loginRules"
-        class="login-form"
-        autocomplete="on"
-        label-position="left"
-      >
-        <div class="title-container">
-          <h3 class="title">
-            深圳市坪山区民生诉求数据分析系统
-          </h3>
-        </div>
+    <a class="logo" href="http://www.szpsq.gov.cn/" target="_blank">
+      <img class="logo-img" src="./components/坪山区人民政府.png" alt="坪山区人民政府">
+    </a>
+    <el-form
+      ref="loginForm"
+      :model="loginForm"
+      :rules="loginRules"
+      class="login-form"
+      autocomplete="on"
+      label-position="left"
+    >
+      <div class="title-container">
+        <h3 class="title">
+          深圳市坪山区民生诉求数据分析系统
+        </h3>
+      </div>
 
-        <el-form-item prop="username">
+      <el-form-item prop="username">
+        <span class="svg-container">
+          <svg-icon icon-class="user" />
+        </span>
+        <el-input
+          ref="username"
+          v-model="loginForm.username"
+          placeholder="Username"
+          name="username"
+          type="text"
+          tabindex="1"
+          autocomplete="on"
+        />
+      </el-form-item>
+
+      <el-tooltip
+        v-model="capsTooltip"
+        content="Caps lock is On"
+        placement="right"
+        manual
+      >
+        <el-form-item prop="password">
           <span class="svg-container">
-            <svg-icon icon-class="user" />
+            <svg-icon icon-class="password" />
           </span>
           <el-input
-            ref="username"
-            v-model="loginForm.username"
-            placeholder="Username"
-            name="username"
-            type="text"
-            tabindex="1"
+            :key="passwordType"
+            ref="password"
+            v-model="loginForm.password"
+            :type="passwordType"
+            placeholder="Password"
+            name="password"
+            tabindex="2"
             autocomplete="on"
+            @keyup.native="checkCapslock"
+            @blur="capsTooltip = false"
+            @keyup.enter.native="handleLogin"
           />
+          <span
+            class="show-pwd"
+            @click="showPwd"
+          >
+            <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
+          </span>
         </el-form-item>
+      </el-tooltip>
 
-        <el-tooltip
-          v-model="capsTooltip"
-          content="Caps lock is On"
-          placement="right"
-          manual
-        >
-          <el-form-item prop="password">
-            <span class="svg-container">
-              <svg-icon icon-class="password" />
-            </span>
-            <el-input
-              :key="passwordType"
-              ref="password"
-              v-model="loginForm.password"
-              :type="passwordType"
-              placeholder="Password"
-              name="password"
-              tabindex="2"
-              autocomplete="on"
-              @keyup.native="checkCapslock"
-              @blur="capsTooltip = false"
-              @keyup.enter.native="handleLogin"
-            />
-            <span
-              class="show-pwd"
-              @click="showPwd"
-            >
-              <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
-            </span>
-          </el-form-item>
-        </el-tooltip>
+      <el-button
+        :loading="loading"
+        type="primary"
+        style="width:100%;margin-bottom:30px;"
+        @click.native.prevent="handleLogin(loginForm)"
+      >
+        登录
+      </el-button>
 
-        <el-button
-          :loading="loading"
-          type="primary"
-          style="width:100%;margin-bottom:30px;"
-          @click.native.prevent="handleLogin(loginForm)"
-        >
-          登录
-        </el-button>
+      <!-- <div style="position:relative">
+          <div class="tips">
+            <span>Username : admin</span>
+            <span>Password : any</span>
+          </div>
+          <div class="tips">
+            <span style="margin-right:18px;">Username : editor</span>
+            <span>Password : any</span>
+          </div>
 
-        <!-- <div style="position:relative">
-            <div class="tips">
-              <span>Username : admin</span>
-              <span>Password : any</span>
-            </div>
-            <div class="tips">
-              <span style="margin-right:18px;">Username : editor</span>
-              <span>Password : any</span>
-            </div>
-
-            <el-button class="thirdparty-button" type="primary" @click="showDialog=true">
-              Or connect with
-            </el-button>
-          </div> -->
-      </el-form>
-    </div>
+          <el-button class="thirdparty-button" type="primary" @click="showDialog=true">
+            Or connect with
+          </el-button>
+        </div> -->
+    </el-form>
 
     <el-dialog
       title="Or connect with"
@@ -322,24 +323,11 @@ $dark_gray:#889aa4;
 $dark_blue: rgb(13, 76, 119);
 $word_col: $dark_blue;
 
-/*.aside {
-  position: absolute;
-  top: 0;
-  right: 0;
-  width: 36%;
-  bottom: 0;
-  transition: {
-    transition-property: width;
-    transition-duration: 0.3s;
-    transition-timing-function: ease-out;
-    transition-delay: 0s;
-  }
-}*/
 .login-container {
   min-height: 100%;
   width: 100%;
   //background-color: $bg;
-  background: url("./components/backgroundImage1.jpg") no-repeat center;
+  background: url("./components/backgroundImage1_gauss.jpg") no-repeat center;
   background-size: cover; //覆盖整个页面
   position: absolute;
   overflow: hidden;
@@ -428,4 +416,11 @@ $word_col: $dark_blue;
     }
   }
 }
+
+.logo {
+  margin-left: 3%;
+  position: absolute;
+  top: 3%
+}
+
 </style>
