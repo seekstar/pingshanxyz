88ad53e56763bcc6f85e0aba08922163768398fb
searchstar <632863986@qq.com>
2019-12-05 17:40:02 +0800

    曲线图美化按钮

--- src/views/dashboard/components/PanelGroup.vue
+++ src/views/dashboard/components/PanelGroup.vue
@@ -1,64 +1,41 @@
 <template>
   <el-col :gutter="40" class="panel-group">
-    <el-row :xs="8" :sm="8" :lg="4" class="card-panel-row">
-      <div class="card-panel" @click="handleSetLineChartData(0)">
-        <div class="card-panel-description">
-          <div class="card-panel-text">
-            碧岭街道
-          </div>
-        </div>
+    <el-button style="padding: 15px 40px;" @click="handleSetLineChartData(0)">
+      <div class="card-panel-text">
+        碧岭街道
       </div>
-    </el-row>
+    </el-button>
+
+    <el-button style="padding: 15px 40px;" @click="handleSetLineChartData(1)">
+      <div class="card-panel-text">
+        龙田街道
+      </div>
+    </el-button>
+
+    <el-button style="padding: 15px 40px;" @click="handleSetLineChartData(2)">
+      <div class="card-panel-text">
+        马峦街道
+      </div>
+    </el-button>
+
+    <el-button style="padding: 15px 40px;" @click="handleSetLineChartData(3)">
+      <div class="card-panel-text">
+        石井街道
+      </div>
+    </el-button>
+
+    <el-button style="padding: 15px 40px;" @click="handleSetLineChartData(4)">
+      <div class="card-panel-text">
+        坪山街道
+      </div>
+    </el-button>
+
+    <el-button style="padding: 15px 40px;" @click="handleSetLineChartData(5)">
+      <div class="card-panel-text">
+        坑梓街道
+      </div>
+    </el-button>
     
-    <el-row :xs="8" :sm="8" :lg="4" class="card-panel-row">
-      <div class="card-panel" @click="handleSetLineChartData(1)">
-        <div class="card-panel-description">
-          <div class="card-panel-text">
-            龙田街道
-          </div>
-        </div>
-      </div>
-    </el-row>
-
-    <el-row :xs="8" :sm="8" :lg="4" class="card-panel-row">
-      <div class="card-panel" @click="handleSetLineChartData(2)">
-        <div class="card-panel-description">
-          <div class="card-panel-text">
-            马峦街道
-          </div>
-        </div>
-      </div>
-    </el-row>
-
-    <el-row :xs="8" :sm="8" :lg="4" class="card-panel-row">
-      <div class="card-panel" @click="handleSetLineChartData(3)">
-        <div class="card-panel-description">
-          <div class="card-panel-text">
-            石井街道
-          </div>
-        </div>
-      </div>
-    </el-row>
-
-    <el-row :xs="8" :sm="8" :lg="4" class="card-panel-row">
-      <div class="card-panel" @click="handleSetLineChartData(4)">
-        <div class="card-panel-description">
-          <div class="card-panel-text">
-            坪山街道
-          </div>
-        </div>
-      </div>
-    </el-row>
-
-    <el-row :xs="8" :sm="8" :lg="4" class="card-panel-row">
-      <div class="card-panel" @click="handleSetLineChartData(5)">
-        <div class="card-panel-description">
-          <div class="card-panel-text">
-            坑梓街道
-          </div>
-        </div>
-      </div>
-    </el-row>
   </el-col>
 </template>
 
@@ -74,38 +51,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.panel-group {
-  .card-panel {
-    height: 50px;
-    cursor: pointer;
-    font-size: 12px;
-    position: relative;
-    overflow: hidden;
-    color: #666;
-    background: #fff;
-    box-shadow: 4px 4px 40px rgba(0, 0, 0, .05);
-    border-color: rgba(0, 0, 0, .05);
 
-    .card-panel-description {
-      float: right;
-      //font-weight: bold;
-      margin: 13px;
-      margin-left: 0px;//???
-
-      .card-panel-text {
-        //line-height: 18px;
-        //color: rgba(0, 0, 0, 0.45);
-        color: rgb(14, 154, 235);
-        font-size: 23px;
-        //margin-bottom: 12px;
-      }
-    }
+.el-button {
+  float: right;
+  .card-panel-text {
+    color: rgb(14, 154, 235);
+    font-size: 23px;
   }
 }
-
-/*@media (max-width:550px) {
-  .card-panel-description {
-    display: none;
-  }
-}*/
 </style>
